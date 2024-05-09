92a9c4e3277608d70a5de3ff710ad72bcd8bcf78
QuentinGra <pilou4260042@gmail.com>
2024-05-09 18:28:14 +0200

    Add enum for status

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -9,47 +9,46 @@ class Starship
         private string $name,
         private string $class,
         private string $captain,
-        private string $status,
+        private StarshipStatusEnum $status,
     ) {
     }
 
-    /**
-     * Get the value of id.
-     */
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * Get the value of name.
-     */
-    public function getName()
+    public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * Get the value of class.
-     */
-    public function getClass()
+    public function getClass(): string
     {
         return $this->class;
     }
 
-    /**
-     * Get the value of captain.
-     */
-    public function getCaptain()
+    public function getCaptain(): string
     {
         return $this->captain;
     }
 
-    /**
-     * Get the value of status.
-     */
-    public function getStatus()
+    public function getStatus(): StarshipStatusEnum
     {
         return $this->status;
+    }
+
+    public function getStatusString(): string
+    {
+        return $this->status->value;
+    }
+
+    public function getStatusImageFilename(): string
+    {
+        return match ($this->status) {
+            StarshipStatusEnum::WAITING => 'images/status-waiting.png',
+            StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
+            StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
+        };
     }
 }
